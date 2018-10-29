071f2f15876670c92512d1c3200d7bf6c4d8084d
David Wood <dw@d4v1d.us>
2018-10-29 14:32:54 -0500

    version 1.1

--- main.py
+++ main.py
@@ -5,21 +5,6 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-@app.route("/crossoff", methods=['POST'])
-def crossoff_movie():
-    crossed_off_movie = request.form['crossed-off-movie']
-
-    if crossed_off_movie not in get_current_watchlist():
-        # the user tried to cross off a movie that isn't in their list,
-        # so we redirect back to the front page and tell them what went wrong
-        error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
-
-        # redirect to homepage, and include error as a query parameter in the URL
-        return redirect("/?error=" + error)
-
-    # if we didn't redirect by now, then all is well
-    return render_template('crossoff.html', crossed_off_movie=crossed_off_movie)
-
 @app.route("/add", methods=['POST'])
 def add_user():
     # get the form values
@@ -76,48 +61,16 @@ def add_user():
             return redirect("/?Email_error=" + error + "&Username=" + Username + "&Email=" + Email) 
 
         check = '@'
-        if Email.count(check) > 1 : 
+        if Email.count(check) != 1 : 
             error = "Invalid Email Address "
             return redirect("/?Email_error=" + error + "&Username=" + Username + "&Email=" + Email)
 
         check = '.'
-        if Email.count(check) > 1 : 
+        if Email.count(check) != 1 : 
             error = "Invalid Email Address "
             return redirect("/?Email_error=" + error + "&Username=" + Username + "&Email=" + Email)
 
     return render_template('welcome.html', Username=Username)
-
-
-    # if (Username == '') or (new_movie.strip() == ""):
-    #     error = "Please specify the movie you want to add."
-    #     return redirect("/?error=" + error)
-
-    # if the user wants to add a terrible movie, redirect and tell them the error
-    # if new_movie in terrible_movies:
-    #     error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
-    #     return redirect("/?error=" + error)
-
-    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-    #new_movie_escaped = cgi.escape(new_movie, quote=True)
-
-    # TODO:
-    # Create a template called add-confirmation.html inside your /templates directory
-    # Use that template to render the confirmation message instead of this temporary message below
-
-    # return render_template('add-confirmation.html', new_movie=new_movie)
-
-   # return "Confirmation Message Under Construction..."
-
-# TODO:
-# Modify the edit.html file to display the watchlist in an unordered list with bullets in front of each movie.
-# Put the list between "Flicklist" and "Edit My Watchlist" under this heading: <h2>My Watchlist</h2>
-
-# TODO:
-# Change get_current_watchlist to return []. This simulates a user with an empty watchlist.
-# Modify edit.html to make sense in such a situation:
-#  First: Hide the <h2>My Watchlist</h2> and it's unordered list.
-#  Second: Hide the crossoff form, since there are no movies to cross off. 
-# Then you can change get_current_watchlist back to the list of hard-coded movies.
 
 @app.route("/")
 def index():
